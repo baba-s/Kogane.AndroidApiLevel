48e11ca010295b8e25e31c11ecfe7b30e3b72365
baba-s <shotababa1206@gmail.com>
2022-11-30 20:05:55 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Kogane.AndroidApiLevel
+# Kogane Android Api Level
+
+アプリケーションを実行している Android 端末の API Level を取得できるパッケージ
+
+## 使用例
+
+```cs
+// アプリケーションを実行している Android 端末の API Level
+Debug.Log( AndroidApiLevel.Value );
+
+// アプリケーションを実行している Android 端末のバージョンが 13 以上なら true
+Debug.Log( AndroidApiLevel.IsAndroidVersion13OrHigher );
+```
---
+++ Runtime/AndroidApiLevel.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Kogane
+{
+    /// <summary>
+    /// アプリケーションを実行している Android 端末の API Level を取得できるクラス
+    /// </summary>
+    public static class AndroidApiLevel
+    {
+        //================================================================================
+        // プロパティ
+        //================================================================================
+        /// <summary>
+        /// アプリケーションを実行している Android 端末の API Level を返します
+        /// </summary>
+        public static int Value
+        {
+            get
+            {
+#if UNITY_EDITOR || UNITY_ANDROID
+                if ( Application.isEditor ) return -1;
+                using var version = new AndroidJavaClass( "android.os.Build$VERSION" );
+                return version.GetStatic<int>( "SDK_INT" );
+#else
+                return -1;
+#endif
+            }
+        }
+
+        /// <summary>
+        /// アプリケーションを実行している Android 端末のバージョンが 13 以上なら true を返します
+        /// </summary>
+        public static bool IsAndroidVersion13OrHigher => 13 <= Value;
+    }
+}
